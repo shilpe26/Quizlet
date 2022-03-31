2b65e8772aba986ff484fcd8eb09fc9e58446d90
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-03-31 13:15:55 +0530

    📄docs: Commit all the basic file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import {Routes, Route} from "react-router-dom";
+import {Navbar, Footer} from "./components/Cmponents";
+import {Home, PageNotFound} from "./pages//Pages";
 function App() {
   return (
     <div className="App">
-      
+      <div>
+      <Navbar />
+      </div>
+      <div className="main-style">
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+      </div>
+      <div>
+    <Footer />
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
