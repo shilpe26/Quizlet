71c388e86af0ac573ebedadb943f6646fed3a7c0
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-04-01 23:43:47 +0530

    refactor: question data

--- src/db/category.js
+++ src/db/category.js
@@ -5,32 +5,28 @@ export const category = [
 		_id: uuid(),
 		categoryName: "DBMS",
 		categoryImage: "https://i.postimg.cc/SX8pVKyR/DBMS.png",
-		description:
-			"Test your knowledge for Database Management System.",
+		description:"🔹Test your knowledge for Database Management System.",
 		categoryValue: "DBMS",
 	},
 	{
 		_id: uuid(),
 		categoryName: "DSA",
 		categoryImage: "https://i.postimg.cc/4YjgzcRj/DSA.jpg",
-		description:
-			"Test your knowledge for Data Structure and Algorithm.",
+		description:"🔹Test your knowledge for Data Structure and Algorithm.",
 		categoryValue: "DSA",
 	},
 	{
 		_id: uuid(),
 		categoryName: "JAVA",
 		categoryImage: "https://i.postimg.cc/ppGxL7GW/java.png",
-		description:
-			"Test your knowledge for JAVA Programming Language.",
+		description:"🔹Test your knowledge for JAVA Programming Language.",
 		categoryValue: "JAVA",
 	},
 	{
 		_id: uuid(),
 		categoryName: "OOPS",
 		categoryImage: "https://i.postimg.cc/FYYvdZ8j/oops.jpg",
-		description:
-			"Test your knowledge for Object-Oriented Programming System (OOPs).",
+		description:"🔹Test your knowledge for Object-Oriented Programming System.",
 		categoryValue: "OOPS",
 	},
 ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,13 +9,13 @@ function Home() {
 			</h1>
 			<section className="cards_for-book cards_for-wish p-8 flex flex-wrap justify-around gapr">
 				{category.map((item) => (
-					<div className="center3 min-h-62" key={item._id}>
+					<div className="center3 min-h-65" key={item._id}>
 						<div className="container2">
 							<div className="img_container2">
 								<img className="img2" src={item.categoryImage} alt="category" />
 								<div className="card">
 									<h2 className="head2-2">{item.categoryName}</h2>
-									<h3 className="head3-2">{item.description}</h3>
+									<h3 className="head3-2 text-md">{item.description}</h3>
 								</div>
 							</div>
 						</div>
