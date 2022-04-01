e329c2e5d57edf65c5388d4a1321192c41f0e69e
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-04-01 19:42:10 +0530

    feat: Page not found file

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import NotFound from "../assets/page_not_found.png";
 function PageNotFound() {
-	return <div>PageNotFound</div>;
+	return (
+		<div className="flex items-center justify-center">
+			<img className="w-80p min-h-65" src={NotFound} alt="not-found" />
+		</div>
+	);
 }
 
 export { PageNotFound };
